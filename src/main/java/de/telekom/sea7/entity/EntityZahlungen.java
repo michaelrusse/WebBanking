b2de6d65eaa6b096af287e1094a0f8d5dcb2a931
Michael Russe <michael.russe@telekom.de>
2022-02-28 11:04:03 +0100

    entity-class angelegt.

---
+++ src/main/java/de/telekom/sea7/entity/EntityZahlungen.java
@@ -0,0 +1,80 @@
+package de.telekom.sea7.entity;
+
+import java.time.LocalDateTime;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class EntityZahlungen {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private float betrag;
+	private String empfaenger;
+	private String iban;
+	private String bic;
+	private String verwendungszweck;
+	private LocalDateTime datum;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public EntityZahlungen() {
+	}
+
+	public float getBetrag() {
+		return betrag;
+	}
+
+	public void setBetrag(float betrag) {
+		this.betrag = betrag;
+	}
+
+	public String getEmpfaenger() {
+		return empfaenger;
+	}
+
+	public void setEmpfaenger(String empfaenger) {
+		this.empfaenger = empfaenger;
+	}
+
+	public String getIban() {
+		return iban;
+	}
+
+	public void setIban(String iban) {
+		this.iban = iban;
+	}
+
+	public String getBic() {
+		return bic;
+	}
+
+	public void setBic(String bic) {
+		this.bic = bic;
+	}
+
+	public String getVerwendungszweck() {
+		return verwendungszweck;
+	}
+
+	public void setVerwendungszweck(String verwendungszweck) {
+		this.verwendungszweck = verwendungszweck;
+	}
+
+	public LocalDateTime getDatum() {
+		return datum;
+	}
+
+	public void setDatum(LocalDateTime datum) {
+		this.datum = datum;
+	}
+}
